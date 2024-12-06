627d0c6f210572576ba3deb556290f769c1363ac
rwx-yxu <yongle.xu@bbc.co.uk>
2024-12-06 12:31:40 +0000

    Scaffold empty program main and remove template program files.

---
+++ src/main/kotlin/Program.kt
@@ -0,0 +1,15 @@
+import org.openrndr.application
+
+fun main() = application {
+    configure {
+        width = 800
+        height = 800
+    }
+
+    program {
+
+        extend {
+
+        }
+    }
+}
